301be7bf4d7c6b20e1de10bd2fd4e1bf57badaac
Lorand98 <kalmarlorand@yahoo.com>
2022-01-11 23:54:51 +0200

    assets/images added icon components created comments styled (grid layout)

--- src/components/Comment/CommentContent.jsx
+++ src/components/Comment/CommentContent.jsx
@@ -1,7 +1,52 @@
 import classes from './Comment.module.scss';
+import IconPlusMinus from '../Icons/IconPlusMinus';
 
 function CommentContent(props) {
-  return <div className={classes['comment__content']}>{props.content}</div>;
+  const userImagePng = props.user.image.png.replace('./', '');
+
+  return (
+    <div className={classes['comment__content']}>
+      <div className={classes['comment__content__score']}>
+        <button className={classes['comment__content__score__btn']}>
+          <IconPlusMinus
+            className={classes['comment__content__score__btn__icon']}
+            plus={true}
+          />
+        </button>
+        <p className={classes['comment__content__score__value']}>
+          {props.score}
+        </p>
+        <button className={classes['comment__content__score__btn']}>
+          <IconPlusMinus
+            className={classes['comment__content__score__btn__icon']}
+            plus={false}
+          />
+        </button>
+      </div>
+      <div className={classes['comment__content__details']}>
+        <img
+          className={classes['comment__content__details__user-image']}
+          src={require(`../../assets/${userImagePng}`)}
+          alt={`${props.user.username} profile picture`}
+        />
+        <p className={classes['comment__content__details__user-name']}>
+          {props.user.username}
+        </p>
+
+        <span className={classes['comment__content__details__created-at']}>
+          {props.createdAt}
+        </span>
+      </div>
+      <p className={classes['comment__content__text']}>
+        {props.replyingTo && (
+          <span className={classes['comment__content__text__replying-to']}>
+            {props.replyingTo}
+          </span>
+        )}{' '}
+        {props.content}
+      </p>
+    </div>
+  );
 }
 
 export default CommentContent;
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
         type: 'asset',
       },
       {
